c3b4400e0b2c5f24d6ae5ba14cbdfabcf8424b76
J <j@optimistic.ninja>
2017-01-22 16:03:15 -0700

    Bash is not a Makefile, J.

--- deploy-tar.sh
+++ deploy-tar.sh
@@ -2,8 +2,8 @@
 
 IMAGE_PERMISSIONS="644"
 SITE_USER="www-data"
-SITE_GROUP="$(SITE_USER)"
-SITE_USERGROUP="$(SITE_USER):$(SITE_GROUP)"
+SITE_GROUP="$SITE_USER"
+SITE_USERGROUP="$SITE_USER:$SITE_GROUP"
 
 if [ -f /tmp/site.tar ]; then
 	echo "Stopping Apache...."
@@ -14,9 +14,9 @@ if [ -f /tmp/site.tar ]; then
 	echo "Extracting new static pages..."
 	tar xvf /tmp/site.tar
 	echo "Modifying owner/group of site..."
-	chown -R $(SITE_USERGROUP) *
+	chown -R $SITE_USERGROUP *
 	echo "Changing permissions for images..."
-	chmod $(IMAGE_PERMISSIONS) public/images/*
+	chmod $IMAGE_PERMISSIONS public/images/*
 	echo "Starting apache..."
 	service apache2 start
 fi
