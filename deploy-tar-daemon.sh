0eab728ef98f8907615baf8ad26fc5714cbbf0a1
J <j@optimistic.ninja>
2017-01-22 22:01:34 -0700

    Added daemonized deploy.

---
+++ deploy-tar-daemon.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+WATCH_FILE="/tmp/site.tar"
+SITE_PATH="/var/www/html/optimistic.ninja/public_html"
+IMAGE_PATH="public/images"
+IMAGE_PERMISSIONS="644"
+SITE_USER="www-data"
+SITE_GROUP="$SITE_USER"
+SITE_USERGROUP="$SITE_USER:$SITE_GROUP"
+
+
+stop_apache() {
+	echo "Stopping Apache...."
+	service apache2 stop
+}
+
+make_backup() {
+	echo "Creating backup..."
+	tar cvf site.tar *
+	mv site.tar ../backups/`date +"%m-%d-%y-%H%M"`-archive.tar
+}
+
+remove_old() {
+	echo "Removing old static pages..."
+	rm -rf *
+}
+
+extract_new() {
+	echo "Extracting new static pages..."
+	tar xvf $1
+}
+
+fix_ownership() {
+	echo "Fixing ownership..."
+	chown -R $SITE_USERGROUP *
+}
+
+fix_permissions() {
+	echo "Fixing image permissions..."
+	chmod $IMAGE_PERMISSIONS $IMAGE_PATH/*
+}
+
+start_apache() {
+	echo "Starting apache..."
+	service apache2 start
+}
+
+cleanup() {
+	rm -rf $WATCH_FILE
+}
+
+# $1 == The location of your tar.
+main() {
+	if [ -f "$WATCH_FILE" ]; then
+		stop_apache	
+		cd $SITE_PATH
+		make_backup
+		remove_old
+		extract_new $1
+		fix_ownership
+		fix_permissions
+		start_apache
+		cleanup
+	fi
+}
+
+main
