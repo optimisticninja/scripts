524ba6905ee451b5f3462ee9da92de6cb4a6d437
J <j@optimistic.ninja>
2017-03-31 12:18:55 -0700

    Modified backup script to be remote only, need to redo locals. Backup list changed for propery home.

--- backup-system.sh
+++ backup-system.sh
@@ -1,47 +1,13 @@
 #!/bin/bash
 
-# 
-# TODO: Test this actually works after revamp/remote server. Damn backup server needs to finish randomizing.
-# 
+#
+# Remote server backup, configure to your liking.
+#
 
-BACKUP_SERVER="cheese"
+BACKUP_SERVER="192.168.1.10"
 BACKUP_PORT="5222"
-BACKUP_USER="backup"
-BACKUP_LISTS="$HOME/scripts/backup-system.includes"
-BACKUP_DIR="/home/backup/"
-ENCRYPTED_DEVICE="/dev/mapper/backup"
-BACKUP_DRIVE_DECRYPTED_NAME="backup"
+BACKUP_USER="jonesy"
+BACKUP_LIST="$HOME/scripts/backup-system.includes"
+BACKUP_DIR="/home/jonesy/backup/"
 
-backup_files() {
-	if [ ${BACKUP_SERVER} = "" ]; then
-		sudo rsync -avzR --files-from=${BACKUP_LIST} ${BACKUP_DIR}
-	else
-		sudo rsync -avzR --files-from=${BACKUP_LIST} ${FILES} "${BACKUP_USER}@${BACKUP_SERVER}:${BACKUP_DIR} -P ${BACKUP_PORT}"
-	fi
-}
-
-mount_decrypted_device() {
-	sudo mount "/dev/mapper/${BACKUP_DRIVE_DECRYPTED_NAME}" ${BACKUP_DIR}
-}
-
-decrypt_backup_drive() {
-	sudo cryptsetup luksOpen ${ENCRYPTED_DEVICE} ${BACKUP_DRIVE_DECRYPTED_NAME}
-}
-
-main() {
-
-	if [ ${BACKUP_SERVER} = "" ]; then
-		if [ ! -e ${ENCRYPTED_DEVICE} ]; then
-			echo "!!! CONNECT BACKUP DRIVE FIRST (Hit enter when ready) !!!"
-			read
-			decrypt_backup_drive
-			mkdir -p ${BACKUP_DIR}
-			mount_decrypted_device
-		fi
-	fi
-
-	create_backup_lists
-	backup_files
-}
-
-main
+sudo rsync -avzR -e "ssh -p $BACKUP_PORT" `cat $BACKUP_LIST` "${BACKUP_USER}@${BACKUP_SERVER}:${BACKUP_DIR}"
