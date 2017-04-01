d1e2823e4c95a78134cf735ea68e3d19b6a37327
J <j@optimistic.ninja>
2017-03-31 22:27:48 -0700

    Fixed encrypted socks tunnel and added verbosity.

--- encrypted-socks-tunnel.sh
+++ encrypted-socks-tunnel.sh
@@ -3,9 +3,9 @@
 USER_AT_HOST="$1"
 PORT="$2"
 
-if [ -z "$USER_AT_HOST"]; then
-	ssh -TND "$PORT" "$USER@HOST"
-elif [ -z "$PORT" ]; then
+if [ ! -z "$USER_AT_HOST" ]; then
+	ssh -vTND "$PORT" "$USER_AT_HOST" 
+elif [ ! -z "$PORT" ]; then
 	echo "USAGE: ./encrypted-socks-tunnel.sh user@server 1337"
 fi
 
